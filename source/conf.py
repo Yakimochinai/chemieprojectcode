56787de6777bc3d5c7e6cc2450ff5dd40996e131
Yakimochinai <89999567+Yakimochinai@users.noreply.github.com>
2024-09-09 05:41:56 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,15 +14,23 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx.ext.autodoc',      # 自动生成文档
+    'sphinx.ext.napoleon',     # 支持 Google 和 NumPy 风格 docstring
+    'sphinx.ext.viewcode',     # 添加源码查看链接
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# 使用 Read the Docs 主题
+html_theme = 'sphinx_rtd_theme'  # 修改为 Read the Docs 默认主题
 html_static_path = ['_static']
+
+# -- Napoleon settings (for Google/NumPy style docstrings) -------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
